0e460fca5b17de7d67069191021cb529ffa7eda5
Ruben Marquez <rubenmfdeveloper@gmail.com>
2022-12-12 16:45:07 +0100

    NEW: Define project architecture
    #ND

---
+++ MemorizationCards/MemorizationCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizationCardsApp.swift
+//  Shared
+//
+//  Created by Ruben Marquez on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizationCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
