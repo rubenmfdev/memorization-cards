8a663ce27a725468d54350917ddf00d56161fee3
Ruben Marquez <rubenmfdeveloper@gmail.com>
2022-12-16 17:04:56 +0100

    NEW: Add CSV library and add different CSVs
    #ND

--- MemorizationCards/Data/DataSources/ProjectStorage/Category/CategoryProjectStorageDataSource.swift
+++ MemorizationCards/Data/DataSources/ProjectStorage/Category/CategoryProjectStorageDataSource.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import SwiftCSV
 
 protocol CategoryProjectStorageDataSourceProtocol {
     func getCategories() -> AnyPublisher<[CategoryEntity], ErrorEntity>
@@ -21,12 +22,36 @@ final class CategoryProjectStorageDataSource: CategoryProjectStorageDataSourcePr
     // MARK: - Implementation
 
     func getCategories() -> AnyPublisher<[CategoryEntity], ErrorEntity> {
-        Just(CategoryEntity.mockList)
-            .setFailureType(to: ErrorEntity.self)
-            .eraseToAnyPublisher()
+        do {
+            var categoryList = [CategoryEntity]()
+            for csvName in getCSVNamesList() {
+                let csvFile: CSV = try CSV<Named>(url: URL(fileURLWithPath: Bundle.main.path(forResource: csvName, ofType: "csv") ?? ""))
+                categoryList.append(createCategoryEntity(with: csvFile, name: csvName))
+            }
+            return Just(categoryList)
+                .setFailureType(to: ErrorEntity.self)
+                .eraseToAnyPublisher()
+        } catch {
+            return Fail(error: ErrorEntity.genericError)
+                .eraseToAnyPublisher()
+        }
     }
 }
 
 private extension CategoryProjectStorageDataSource {
+    func getCSVNamesList() -> [String] {
+        ["irregular verbs", "animals", "cloths", "colors", "food", "fruits", "professions", "verbs"]
+    }
     
+    func createCategoryEntity(with csv: CSV<Named>, name: String) -> CategoryEntity {
+        return CategoryEntity(id: UUID().uuidString, name: name, exercices: self.rowsToTuple(rows: csv.rows))
+    }
+    
+    func rowsToTuple(rows: [[String: String]]) -> [(String, String)] {
+        var tupleList = [(String, String)]()
+        for row in rows {
+            tupleList.append((row["First"] ?? "", row["Second"] ?? ""))
+        }
+        return tupleList
+    }
 }
